265af982096a1f855655a1732a26c7d6e75c2afb
Jose Luis Ametller <jametller@netquest.com>
2017-03-23 15:55:00 +0100

    Initial version of the PoC

---
+++ app/src/main/java/com/netquest/chromecustomtabspoc/MainActivity.java
@@ -0,0 +1,46 @@
+package com.netquest.chromecustomtabspoc;
+
+import android.graphics.Color;
+import android.net.Uri;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		Button goButton = (Button)findViewById(R.id.goButton);
+		goButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Uri uri = Uri.parse("https://www.w3schools.com/js/tryit.asp?filename=tryjs_cookie_username");
+
+				// create an intent builder
+				CustomTabsIntent.Builder intentBuilder = new CustomTabsIntent.Builder();
+
+				// Begin customizing
+				// set toolbar colors
+				intentBuilder.setToolbarColor(ContextCompat.getColor(MainActivity.this, R.color.colorPrimary));
+				intentBuilder.setSecondaryToolbarColor(ContextCompat.getColor(MainActivity.this, R.color.colorPrimaryDark));
+
+				// set start and exit animations
+				intentBuilder.setStartAnimations(MainActivity.this, R.anim.slide_in_right, R.anim.slide_out_left);
+				intentBuilder.setExitAnimations(MainActivity.this, android.R.anim.slide_in_left,
+						android.R.anim.slide_out_right);
+
+				// build custom tabs intent
+				CustomTabsIntent customTabsIntent = intentBuilder.build();
+
+				// launch the url
+				customTabsIntent.launchUrl(MainActivity.this, uri);
+			}
+		});
+	}
+}
